0687f1723a7b113f9896b8145bb6fbb80c8e15f4
baldplayer123 <bouseksouyanis@gmail.com>
2026-06-06 10:36:43 +0200

    Fixing users and rootpassword disabled

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -21,10 +21,13 @@
 	programs.zsh.enable = true;
 
 	#users
+	users.mutableUsers = false;
+	users.users.root.hashedPassword = "!";
 	users.users.bald = {
 		isNormalUser = true;
 		extraGroups = [ "wheel" "networkmanager" "video" "audio"];
 		shell = pkgs.zsh;
+		initialPassword = changeme;
 	};
 
 	# Services
