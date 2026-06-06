88624696ac534abdd439b69372bce90b2c46c63f
baldplayer123 <bouseksouyanis@gmail.com>
2026-06-06 22:55:34 +0200

    fix typo

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
 				home-manager.nixosModules.home-manager {
 					home-manager.useGlobalPkgs = true;
 					home-manager.useUserPackages = true;
-					home-manager.user.bald = import ./hosts/laptop/home.nix;
+					home-manager.users.bald = import ./hosts/laptop/home.nix;
 
 				}
 			];
