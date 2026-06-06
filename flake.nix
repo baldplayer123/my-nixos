734e948fd2fdf0eb8ec0b4be31ba4c88f4a22197
baldplayer123 <bouseksouyanis@gmail.com>
2026-06-06 09:17:55 +0200

    Fix typos on flake.nix

--- flake.nix
+++ flake.nix
@@ -5,16 +5,16 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 		disko = {
 			url = "github:nix-community/disko";
-			inputs.nixpkgs.follow = "nixpkgs";
+			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
 	};
 	
 	## This is going to create a configuration for our linux system 
 	outputs = {self, nixpkgs, disko, ...}: {
-		nixosConfiguration.laptop = nixpkgs.lib.nixosSystem {
+		nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
-			module = [
+			modules = [
 				disko.nixosModules.disko
 				./hosts/laptop/configuration.nix ## Our os configuration
 				./hosts/laptop/disko.nix ## our partitioning configuration
